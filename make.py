6774d41818da3663d909273c6a2e42fefdc801c5
Yoav321 <yoav.bierkatz@gmail.com>
2026-09-17 11:26:13 -0600

    complete the make

--- make.py
+++ make.py
@@ -4,7 +4,18 @@ First Last - Month Year
 """
 
 def main() -> None:
-    pass
+    #input
+    first: str = input("First name: ")
+    middle: str = input("Middle name: ")
+    last: str = input("Last name: ")
+    if len(middle) > 0:
+        username: str = first[0] + middle[0] + last
+    else:
+        username: str = first[0] + last
+    username = username.lower()
+    username = username.replace(" ", "-")
+    username = username.replace("'", "")
+    print(username)
 
 if __name__ == "__main__":
     main()
